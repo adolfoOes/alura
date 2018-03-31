4ff443d0bbdee132bca7ecda977a375e608bceb9
Adolfo <adolfo.oes@gmail.com>
2018-03-31 14:20:22 -0300

    alterações requisições

--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -4,6 +4,15 @@ module.exports = function(app){
     app.get('/pagamentos', function(req, res){
         console.log('Imprimir lista de pagamentos.');
         res.send('OK');
+    });
+
+    app.post('/pagamentos/pagamento', function(req, res){
+
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('OK');
+        
     });    
+
 };
 
